0fdf6e14225e07896955b2fff42a22a7d5a0c48f
abdallahAlabed <abdallah.s.alabd@gmail.com>
2021-04-04 16:13:47 +0300

    new

---
+++ javascript/main.js
@@ -0,0 +1,11 @@
+let name = prompt("what is your name ", "abdallah");
+
+let q2 = prompt("do like European or Asian cars", "European");
+
+let countryManufacturer = prompt("What is the name of the country of manufacturer", "Germany");
+
+let carName = prompt("what is car brands u want", "mercedes ");
+
+alert("your name is " + name + " you like " + q2 + " cars " + countryManufacturer + " is the country of manufacturer your car " + carName + " is your car ");
+
+console.log(typeof q2);
